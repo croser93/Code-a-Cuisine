3eddaa2cadd822ce6b180bed91d8310a6e996bff
Maik G <maik.g@outlook.com>
2026-03-02 19:38:00 +0100

    Add: Footer and Header | Border Var.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './features/components/hero-component/hero-component';
+import { Header } from './features/shared/header/header';
+import { Footer } from './features/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
