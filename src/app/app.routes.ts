2a04161ee95653c476af59b642d6524a096cadcf
Maik G <maik.g@outlook.com>
2026-03-02 10:32:52 +0100

    Add: Styling the Hero Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './features/components/hero-component/hero-component';
 import { RecipeComponent } from './features/components/recipe-component/recipe-component';
 import { PreferenceComponent } from './features/components/preference-component/preference-component';
+import { ResultComponent } from './features/components/result-component/result-component';
+import { CookbookComponent } from './features/components/cookbook-component/cookbook-component';
 
 export const routes: Routes = [
     { path: '', component: HeroComponent },
-    { path: '/recipe', component: RecipeComponent },
-    { path: '/preferences', component: PreferenceComponent },
-    { path: '/results', component: PreferenceComponent },
-    { path: '/cookbook', component: PreferenceComponent },
+    { path: 'recipe', component: RecipeComponent },
+    { path: 'preferences', component: PreferenceComponent },
+    { path: 'results', component: ResultComponent},
+    { path: 'cookbook', component: CookbookComponent},
     { path: '**', redirectTo: '' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './features/components/hero-component/hero-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
