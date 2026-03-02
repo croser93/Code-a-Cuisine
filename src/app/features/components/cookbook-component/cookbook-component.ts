5a7c0f166f69d92d24a5ca2dc8dba797b3b042fe
Maik G <maik.g@outlook.com>
2026-03-02 08:18:08 +0100

    first commit

---
+++ src/app/features/components/cookbook-component/cookbook-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookbook-component',
+  imports: [],
+  templateUrl: './cookbook-component.html',
+  styleUrl: './cookbook-component.scss',
+})
+export class CookbookComponent {
+
+}
---
+++ src/app/features/components/preference-component/preference-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preference-component',
+  imports: [],
+  templateUrl: './preference-component.html',
+  styleUrl: './preference-component.scss',
+})
+export class PreferenceComponent {
+
+}
