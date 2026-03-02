010b43eaee29fcfd209984049a43aa68638aab4e
Maik G <maik.g@outlook.com>
2026-03-02 08:42:59 +0100

    Add: Routes path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/components/hero-component/hero-component';
+import { RecipeComponent } from './features/components/recipe-component/recipe-component';
+import { PreferenceComponent } from './features/components/preference-component/preference-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeroComponent },
+    { path: '/recipe', component: RecipeComponent },
+    { path: '/preferences', component: PreferenceComponent },
+    { path: '/results', component: PreferenceComponent },
+    { path: '/cookbook', component: PreferenceComponent },
+    { path: '**', redirectTo: '' },
+];
