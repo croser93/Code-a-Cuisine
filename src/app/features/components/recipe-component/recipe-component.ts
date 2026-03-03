b2d45b5bf140208a0e6f2d14de9af7388718a05a
Maik G <maik.g@outlook.com>
2026-03-03 10:09:14 +0100

    Add: Ingredient list with dropdown menu

--- src/app/features/components/recipe-component/recipe-component.ts
+++ src/app/features/components/recipe-component/recipe-component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recipe-component.html',
   styleUrl: './recipe-component.scss',
 })
 export class RecipeComponent {
+  amount: number = 100;
+  unit: string = 'gram';
+
+saveData() {
+  console.log(`Gespeichert: ${this.amount} ${this.unit}`);
+}
 
 }
