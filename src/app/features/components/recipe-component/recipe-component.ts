2fbf43c7da236a56055d7976d79c827630b7339f
Maik G <maik.g@outlook.com>
2026-03-03 11:00:21 +0100

    Add: Template for ingredient and push in Ingredientlist

--- src/app/features/components/recipe-component/recipe-component.ts
+++ src/app/features/components/recipe-component/recipe-component.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+
+interface Ingredient {
+  value: string;
+  size: number;
+  unit: string;
+}
 
 @Component({
   selector: 'app-recipe-component',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './recipe-component.html',
   styleUrl: './recipe-component.scss',
 })
 export class RecipeComponent {
   amount: number = 100;
   unit: string = 'gram';
+  value: string = '';
 
-saveData() {
-  console.log(`Gespeichert: ${this.amount} ${this.unit}`);
-}
+  ingredientsList: Ingredient[] = [];
 
+  saveIngredient() {
+
+    const newIngredient: Ingredient = {
+      value: this.value,   
+      size: this.amount,     
+      unit: this.unit        
+    };
+
+    this.ingredientsList.push(newIngredient);
+    this.value = ''; 
+    
+    console.log('Liste aktuell:', this.ingredientsList);
+  }
 }
