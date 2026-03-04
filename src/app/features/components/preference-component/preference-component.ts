bfaa0dd0761c0bc9b6416d2fe0090ba730cd987f
Maik G <maik.g@outlook.com>
2026-03-04 12:39:58 +0100

    feat: add recipe input and preference selection components with routing and icons.

--- src/app/features/components/preference-component/preference-component.ts
+++ src/app/features/components/preference-component/preference-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-preference-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './preference-component.html',
   styleUrl: './preference-component.scss',
 })
