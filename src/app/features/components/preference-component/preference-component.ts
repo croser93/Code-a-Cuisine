5fab35bd49e509cdb255e8ef7eddf26728268f0c
Maik G <maik.g@outlook.com>
2026-03-06 11:29:03 +0100

    Add: function fewer and addedMore fpr portions and person

--- src/app/features/components/preference-component/preference-component.ts
+++ src/app/features/components/preference-component/preference-component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-preference-component',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './preference-component.html',
   styleUrl: './preference-component.scss',
 })
 export class PreferenceComponent {
+  portions: number = 5;
+  person: number = 5;
+
+  addedMore(type: 'portions' | 'person') {
+    if(this[type] >= 1 && this[type] <= 19)
+    this[type]++;
+  }
+  
+  fewer(type: 'portions' | 'person'){
+    if(this[type] > 1)
+      this[type]--;
+  }
+
+    
+  
 
 }
