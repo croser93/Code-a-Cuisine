00db6f724f4fd6dd3f39f6f0146585729cf91416
Maik G <maik.g@outlook.com>
2026-03-06 14:04:29 +0100

    Add: 5sec navigate to results page

--- src/app/features/shared/loading-screen/loading-screen.ts
+++ src/app/features/shared/loading-screen/loading-screen.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loading-screen',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './loading-screen.scss',
 })
 export class LoadingScreen {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+    this.startRedirectTimer();
+  }
+
+  startRedirectTimer() {
+    setTimeout(() => {
+
+      this.router.navigate(['/results']); 
+    }, 5000);
+  }
+
 
 }
