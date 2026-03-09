51a265863035e4e60b1fc5df1fead07f4ce1613b
Maik G <maik.g@outlook.com>
2026-03-09 09:47:46 +0100

    Add: logik cards and styling

--- src/app/features/components/result-component/result-component.ts
+++ src/app/features/components/result-component/result-component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Recipe {
+  text: string;
+  time: string;
+}
+
 
 @Component({
   selector: 'app-result-component',
-  imports: [],
+   imports: [FormsModule, CommonModule],
   templateUrl: './result-component.html',
   styleUrl: './result-component.scss',
 })
+
+
 export class ResultComponent {
 
+   recipeList: Recipe[] = [
+    {text: "pasta with spinach and cherry tomatoes",   
+      time: "20",     
+  },
+      {text: "Creamy garlic shrimp pasta",   
+      time: "22",     
+  },
+      {text: "Pasta alla trapanese (Sicilian Tomato Pesto)",   
+      time: "40",     
+  },
+  ];
 }
