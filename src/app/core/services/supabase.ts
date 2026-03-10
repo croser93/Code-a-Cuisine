5567a3a613c3d6da4f71fc6e0e220e6de3a9dfbf
Maik G <maik.g@outlook.com>
2026-03-10 12:56:51 +0100

    Add: function to push preferences and Ingredients to superbase

--- src/app/core/services/supabase.ts
+++ src/app/core/services/supabase.ts
@@ -2,22 +2,56 @@ import { Injectable, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js'
 import { environment } from '../../../environments/environment.development';
 
+export interface Ingredient {
+  value: string;
+  size: number;
+  unit: string;
+}
+
+export interface UserPreferences {
+  portions: number;
+  person: number;
+  selectedCookingTime: string | null;
+  selectedCuisines: string | null;
+  selectedDietPreference: string | null;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class Supabase {
-supabaseUrl = environment.supabaseUrl
-supabaseKey = environment.supabaseKey
-supabase = createClient(this.supabaseUrl, this.supabaseKey)
+  supabaseUrl = environment.supabaseUrl
+  supabaseKey = environment.supabaseKey
+  supabase = createClient(this.supabaseUrl, this.supabaseKey)
 
-ingredientsAndPreferences = signal<{id:number, created_at:string, Name:string}[]> ([])
+  ingredientsAndPreferences = signal<{ id: number, created_at: string, Name: string }[]>([])
 
-async getProducts(){
-let { data: products, error } = await this.supabase
-  .from('Ingredients and Preferences')
-  .select('*')
-  if(!products) return
-  this.ingredientsAndPreferences.set(products)
-}
-  
+  currentIngredients: Ingredient[] = [];
+
+  // async getProducts() {
+  //   let { data: products, error } = await this.supabase
+  //     .from('Ingredients and Preferences')
+  //     .select('*')
+  //   if (!products) return
+  //   this.ingredientsAndPreferences.set(products)
+  // }
+
+  async pushData(preferences: UserPreferences) {
+    const combinedData = {
+      ingredients: this.currentIngredients,
+      portions: preferences.portions,
+      person: preferences.person,
+      selectedCookingTime: preferences.selectedCookingTime,
+      selectedCuisines: preferences.selectedCuisines,
+      selectedDietPreference: preferences.selectedDietPreference
+    };
+    console.log(combinedData)
+    const { data, error } = await this.supabase
+      .from('Ingredients and Preferences')
+      .insert([
+        combinedData
+      ])
+      .select()
+  }
+
 }
