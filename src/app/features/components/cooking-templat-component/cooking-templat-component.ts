01850be5cf29db0b0b3b4893b8f9de20f769b1eb
Maik G <maik.g@outlook.com>
2026-03-10 07:40:28 +0100

    Add: function to loop the cookingIcon at the number of jyson number

--- src/app/features/components/cooking-templat-component/cooking-templat-component.ts
+++ src/app/features/components/cooking-templat-component/cooking-templat-component.ts
@@ -7,7 +7,7 @@ interface InfoItem {
   diet?: string;
   cookingtime?: string;
   likes?: number;
-  poeple: number;
+  people: number;
 }
 
 interface NutriItem {
@@ -45,51 +45,56 @@ interface RecipeIngredients {
 })
 export class CookingTemplatComponent {
 
-cookingIngredientList: RecipeIngredients = {
-  info: {
-    time: "20",
-    diet: "vegetarian" ,
-    cookingtime: "quick" ,
-    likes: 2 ,
-    poeple: 2,
-  },
 
-  nutriInfo: {
-     energie: "80kcal" ,
-     protein: "100g" ,
-     fat: "150g" ,
-     carbs: "100g" 
-  },
+getCookIcons(count: number = 0): number[] {
+  return new Array(Math.min(count, 4)).fill(0);
+}
 
-  yourIngredients: [
-    { size: "80g", text: "Pasta noodles" },
-    { size: "100g", text: "Baby spinach" },
-    { size: "150g", text: "Cherry tomatoes" },
-    { size: "1 piece", text: "Egg" }
-  ],
-
-  extraIngredients: [
-    { size: "40g", text: "Parmesan cheese" },
-    { size: "30ml", text: "Olive oil" },
-  ],
-
-  CookingtextList: [
-    {
-      overhead: "Cook the pasta",
-      step: "Cook your noodles in boiling, salted water, until the pasta is al dente. Drain the pasta and reserve some of the pasta water."
+  cookingIngredientList: RecipeIngredients = {
+    info: {
+      time: "20",
+      diet: "vegetarian",
+      cookingtime: "quick",
+      likes: 2,
+      people: 3,
     },
-    {
-      overhead: "Make the sauce",
-      step: "While the pasta is cooking, heat olive oil in a pan over medium heat. Add the garlic, and sauté until it starts to turn golden. Add the tomatoes, oregano, salt, and pepper, and cook for 3-4 minutes."
+
+    nutriInfo: {
+      energie: "80kcal",
+      protein: "100g",
+      fat: "150g",
+      carbs: "100g"
     },
-    {
-      overhead: "Finish the pasta",
-      step: "Add the noodles to the sauce, then add pasta water until the sauce is the right consistency. Simmer for 1 minute, then add the spinach, basil, chili flakes, and parmesan."
-    },
-    {
-      overhead: "Serve",
-      step: "Lower the heat to low, stir until mixed, and remove from the heat. Season to taste, top with parmesan cheese, and enjoy."
-    }
-  ]
-};
+
+    yourIngredients: [
+      { size: "80g", text: "Pasta noodles" },
+      { size: "100g", text: "Baby spinach" },
+      { size: "150g", text: "Cherry tomatoes" },
+      { size: "1 piece", text: "Egg" }
+    ],
+
+    extraIngredients: [
+      { size: "40g", text: "Parmesan cheese" },
+      { size: "30ml", text: "Olive oil" },
+    ],
+
+    CookingtextList: [
+      {
+        overhead: "Cook the pasta",
+        step: "Cook your noodles in boiling, salted water, until the pasta is al dente. Drain the pasta and reserve some of the pasta water."
+      },
+      {
+        overhead: "Make the sauce",
+        step: "While the pasta is cooking, heat olive oil in a pan over medium heat. Add the garlic, and sauté until it starts to turn golden. Add the tomatoes, oregano, salt, and pepper, and cook for 3-4 minutes."
+      },
+      {
+        overhead: "Finish the pasta",
+        step: "Add the noodles to the sauce, then add pasta water until the sauce is the right consistency. Simmer for 1 minute, then add the spinach, basil, chili flakes, and parmesan."
+      },
+      {
+        overhead: "Serve",
+        step: "Lower the heat to low, stir until mixed, and remove from the heat. Season to taste, top with parmesan cheese, and enjoy."
+      }
+    ]
+  };
 }
