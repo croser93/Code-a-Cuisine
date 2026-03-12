9dd163ea7af14c7c3f65f39cfa76379a0f8caa36
Maik G <maik.g@outlook.com>
2026-03-12 19:41:26 +0100

    feat: Implement cookbook and cooking template components and add the italian.svg asset.

--- src/app/features/components/cookbook-component/cookbook-component.ts
+++ src/app/features/components/cookbook-component/cookbook-component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface Recipe {
+  title: string;
+  time: string;
+  likes: number;
+}
+
+interface Cuisine {
+  name: string;
+  image: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-cookbook-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './cookbook-component.html',
   styleUrl: './cookbook-component.scss',
 })
 export class CookbookComponent {
+  topRecipes: Recipe[] = [
+    { title: 'Pasta with spinach and cherry tomatoes', time: '20min', likes: 66 },
+    { title: 'Low Carb Vegan No-Bake Paleo Bars', time: '35min', likes: 57 },
+    { title: 'Schnitzel with fries', time: '45min', likes: 89 }
+  ];
 
+  cuisines: Cuisine[] = [
+    { name: 'Italian cuisine', image: 'italian.svg', link: '#' },
+    { name: 'German cuisine', image: 'german.svg', link: '#' },
+    { name: 'Japanese cuisine', image: 'japanese.svg', link: '#' },
+    { name: 'Gourmet cuisine', image: 'gourmet.svg', link: '#' },
+    { name: 'Indian cuisine', image: 'indian.svg', link: '#' },
+    { name: 'Fusion cuisine', image: 'fusion.svg', link: '#' }
+  ];
 }
