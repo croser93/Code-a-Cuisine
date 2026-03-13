ab1f0d52adaef4636fec8fa4b3ec4bfa5803c79f
Maik G <maik.g@outlook.com>
2026-03-13 10:47:02 +0100

    Feat: Ui components with dummy data

--- src/app/features/components/cookbook-list-component/cookbook-list-component.ts
+++ src/app/features/components/cookbook-list-component/cookbook-list-component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { CommonModule } from '@angular/common';
 import { Supabase } from '../../../core/services/supabase';
 
 
 @Component({
   selector: 'app-cookbook-list-component',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './cookbook-list-component.html',
   styleUrl: './cookbook-list-component.scss',
 })
 export class CookbookListComponent {
+  dummyRecipes = Array.from({ length: 15 }, (_, i) => ({
+    vegetarian: i % 3 === 0,
+    quick: true
+  }));
 
   constructor(private supabase: Supabase) {};
 
