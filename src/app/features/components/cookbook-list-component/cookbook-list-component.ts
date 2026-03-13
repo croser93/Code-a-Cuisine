218b00d412a64a8b50926331d50f556c710f42c7
Maik G <maik.g@outlook.com>
2026-03-13 09:30:59 +0100

    Add: cookbook-list- coponent and new separator

---
+++ src/app/features/components/cookbook-list-component/cookbook-list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookbook-list-component',
+  imports: [],
+  templateUrl: './cookbook-list-component.html',
+  styleUrl: './cookbook-list-component.scss',
+})
+export class CookbookListComponent {
+
+}
